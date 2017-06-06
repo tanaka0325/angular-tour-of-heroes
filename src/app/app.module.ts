36dff48abdda1dee920205fa92deaf5ac016f013
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-06-06 12:21:20 +0900

    Create app-routing file from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
-import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { DashboardComponent }  from './Dashboard.component';
-import { HeroesComponent }  from './heroes.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }  from './heroes.component';
 
 import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -40,9 +23,7 @@ import { HeroService } from './hero.service';
     HeroesComponent,
     HeroDetailComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
